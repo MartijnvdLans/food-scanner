4016fa1919cf277b2f5c0941c458f173ffbe5dac
MartijnvdLans <martijnvanderlans.00@gmail.com>
2022-02-08 13:17:29 +0100

    kaartje update

--- visitekaartje/scripts/java.js
+++ visitekaartje/scripts/java.js
@@ -2,32 +2,39 @@ var kaart = document.querySelector(".seal");
 var openKaartje = document.querySelector(".driehoek_boven");
 var visiteKaartje = document.querySelector(".visitekaartje");
 var visiteKaartjeInner = document.querySelector(".inner_section");
-
 var kaartOpen = false;
-
 var htmlKnop = document.querySelector(".htmlb");
 var cssKnop = document.querySelector(".cssb");
 var jsKnop = document.querySelector(".javasb");
-
 var kader = document.querySelector(".kader");
-
 var htmlEnabled = false;
 var cssEnabled = false;
 var jsEnabled = false;
+var audio = new Audio('./envelop2.wav');
 
 function openKaart() {
     if (kaartOpen === true) {
         openKaartje.classList.remove("open_me");
-        visiteKaartje.classList.remove("delay");
         visiteKaartjeInner.classList.remove("zoom_in");
+        openKaartje.classList.add("close_me");
+        visiteKaartjeInner.classList.add("zoom_out");
         kaartOpen = false;
+        setTimeout(indexChange, 1500)
     }
     else {
+        openKaartje.classList.remove("close_me");
+        visiteKaartjeInner.classList.remove("zoom_out");
         openKaartje.classList.add("open_me");
         visiteKaartje.classList.add("delay");
         visiteKaartjeInner.classList.add("zoom_in");
         kaartOpen = true;
+        audio.play();
     }
+}
+
+function indexChange() {
+    visiteKaartje.classList.remove("delay");
+    audio.play();
 }
 
 // function sluitKaart() {
