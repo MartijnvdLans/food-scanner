ca09e4e739187b9895e022df8085d264ed9459fd
MartijnvdLans <martijnvanderlans.00@gmail.com>
2022-02-14 14:21:02 +0100

    Camera test

--- FoodApp/js/js.js
+++ FoodApp/js/js.js
@@ -1,10 +1,9 @@
  var video = document.querySelector("#videoElement");
 
 if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({ video: true })
+  navigator.mediaDevices.getUserMedia({ video: {facingMode: "environment"} })
     .then(function (stream) {
       video.srcObject = stream;
-      video.facingMode = "environment"
     })
     .catch(function (err0r) {
       console.log("Something went wrong!");
