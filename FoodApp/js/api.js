546911423c2b54a65e74d2a3b1510536e4ad784b
MartijnvdLans <martijnvanderlans.00@gmail.com>
2022-02-14 11:14:24 +0100

    barcode test

---
+++ FoodApp/js/api.js
@@ -0,0 +1,22 @@
+if (!('BarcodeDetector' in window)) {
+    console.log('Barcode Detector is not supported by this browser.');
+  } else {
+    console.log('Barcode Detector supported!');
+  
+    // create new detector
+    var barcodeDetector = new BarcodeDetector({formats: ['code_39', 'codabar', 'ean_13', 'ean_8']});
+  }
+
+  // check supported types
+BarcodeDetector.getSupportedFormats()
+.then(supportedFormats => {
+  supportedFormats.forEach(format => console.log(format));
+});
+
+barcodeDetector.detect(imageEl)
+    .then(barcodes => {
+      barcodes.forEach(barcode => console.log(barcode.rawData));
+    })
+    .catch(err => {
+      console.log(err);
+    })
