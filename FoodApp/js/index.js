e82bae83541883e7da40878a35186a240c9b6abd
MartijnvdLans <martijnvanderlans.00@gmail.com>
2022-02-14 15:19:59 +0100

    Werkt het?

--- FoodApp/js/index.js
+++ FoodApp/js/index.js
@@ -1,99 +1,72 @@
 window.onload = () => {
-    detect();
-  };
-  
-  async function detect() {
-    const barcodeDetector = new BarcodeDetector();
-    const list = document.getElementById("barcodes");
-    let itemsFound = [];
-    const mediaStream = await navigator.mediaDevices.getUserMedia({
-      video: { facingMode: "environment" }
-    });
-  
-    const video = document.createElement("video");
-    video.srcObject = mediaStream;
-    video.autoplay = true;
-    video.videoWidth = "300px"
-    video.videoHeight = "300px"
-  
-    list.before(video);
-  
-    function render() {
-      barcodeDetector
-        .detect(video)
-        .then((barcodes) => {
-          barcodes.forEach((barcode) => {
-            if (!itemsFound.includes(barcode.rawValue)) {
-              itemsFound.push(barcode.rawValue);
-              const li = document.createElement("li");
-              li.innerHTML = barcode.rawValue;
-              const newBarcode = barcode.rawValue; 
-              list.appendChild(li);
-              const endpoint = 'https://world.openfoodfacts.org/api/v0/product/' + newBarcode + '.json';
-              fetchData(endpoint).then(data => {
-                console.log(data)
-                // .insertAdjacentHTML('afterbegin', '<h1>' + data.product['nutriscore_data'] +'</h1>')
-            
+  detect();
+};
+
+
+async function detect() {
+  const barcodeDetector = new BarcodeDetector();
+  const list = document.getElementById("barcodes");
+  let itemsFound = [];
+  const mediaStream = await navigator.mediaDevices.getUserMedia({
+    video: { facingMode: "environment" }
+  });
+
+  const video = document.createElement("video");
+  video.srcObject = mediaStream;
+  video.autoplay = true;
+
+  list.before(video);
+
+
+  function render() {
+    barcodeDetector
+      .detect(video)
+      .then((barcodes) => {
+        barcodes.forEach((barcode) => {
+          if (!itemsFound.includes(barcode.rawValue)) {
+            itemsFound.push(barcode.rawValue);
+            const li = document.createElement("li");
+            li.innerHTML = barcode.rawValue;
+            const newBarcode = barcode.rawValue; 
+            list.appendChild(li);
+            const getURL = 'https://world.openfoodfacts.org/api/v0/product/' + newBarcode+ '.json'
+            fetch(getURL).then(response => response.json())
+            .then(response => {
+                console.log(response.product)
+        
+                const product = {
+                    name: response.product.product_name,
+                    brand: response.product.brand_owner,
+                    nutriscore: response.product.nutrient_levels.fat,
+                    img: response.product.image_front_url
+                }
+        
                 const markup = `
-                <img src="${data.product['image_front_url']}">
-                <h2>
-                    ${data.product['product_name']}
-                </h2>
-                <ul>
-                    <li>Kcal: ${data.product['nutriments'].energy}</li>
-                    <li>Vetten: ${data.product['nutriments'].fat}</li>
-                    <li>Vezels: ${data.product['nutriments'].fiber}</li>
-                </ul>
-                `;
-            
-                document.body.querySelector('.app').innerHTML = markup;
+         <div class="person">
+                <img src=${product.img}>
+                <h2>${product.name} </h2>
+            <h3>
+                ${product.brand}
+            </h3>
+            <p class="location">${product.nutriscore}</p>
+         </div>
+        `;
+        
+        document.querySelector("main section:first-of-type").innerHTML = markup;    
             })
-            }
-          });
-        })
-        .catch(console.error);
-    }
-
-  
-    (function renderLoop() {
-      requestAnimationFrame(renderLoop);
-      render();
-    })();
+            .catch(error => document.body.insertAdjacentHTML('beforebegin', error))
+          }
+        });
+      })
+      .catch(console.error);
   }
-  
-//   console.log('are we connected?')
 
-// let barcode = '737628064502'
-// const endpoint = 'https://world.openfoodfacts.org/api/v0/product/' + barcode + '.json';
+// barcode getter
 
+// const barcode = "li.innerHTML";
 
-// async function fetchData(url) {
-//     try {
-//         const response = await fetch(url)
-//         const data = await response.json()
-//         return data;
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
-
-
-
-// fetchData(endpoint).then(data => {
-//     console.log(data)
-//     // .insertAdjacentHTML('afterbegin', '<h1>' + data.product['nutriscore_data'] +'</h1>')
-
-//     const markup = `
-//     <img src="${data.product['image_front_url']}">
-//     <h2>
-//         ${data.product['product_name']}
-//     </h2>
-//     <ul>
-//         <li>Kcal: ${data.product['nutriments'].energy}</li>
-//         <li>Vetten: ${data.product['nutriments'].fat}</li>
-//         <li>Vezels: ${data.product['nutriments'].fiber}</li>
-//     </ul>
-//     `;
-
-//     document.body.querySelector('.app').innerHTML = markup;
-// })
+  (function renderLoop() {
+    requestAnimationFrame(renderLoop);
+    render();
+  })();
+}
