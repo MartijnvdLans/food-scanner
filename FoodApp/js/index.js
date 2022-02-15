10a307fa7638fa22021f5119be11eef9e12e2324
MartijnvdLans <martijnvanderlans.00@gmail.com>
2022-02-15 10:57:09 +0100

    Test focus

--- FoodApp/js/index.js
+++ FoodApp/js/index.js
@@ -8,7 +8,7 @@ async function detect() {
   const list = document.getElementById("barcodes");
   let itemsFound = [];
   const mediaStream = await navigator.mediaDevices.getUserMedia({
-    video: {facingMode: "environment"}
+    video: {facingMode: { ideal: "environment"}}
   });
 
   const video = document.createElement("video");
