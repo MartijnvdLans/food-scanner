b4c873dee58ddeb48f5dd464b83b31ebb09778f4
MartijnvdLans <martijnvanderlans.00@gmail.com>
2022-02-15 11:23:33 +0100

    test

--- FoodApp/js/index.js
+++ FoodApp/js/index.js
@@ -7,17 +7,83 @@ async function detect() {
   const barcodeDetector = new BarcodeDetector();
   const list = document.getElementById("barcodes");
   let itemsFound = [];
-  const mediaStream = await navigator.mediaDevices.getUserMedia({
-    video: {facingMode: { ideal: "environment"}, zoom: true}
+  // const mediaStream = await navigator.mediaDevices.getUserMedia({
+  //   video: {facingMode: { ideal: "environment"}, zoom: true}
+  // });
+
+  // const video = document.createElement("video");
+  // video.srcObject = mediaStream;
+  // video.autoplay = true;
+  // video.videoWidth = "300px";
+  // video.videoHeight = "300px"
+
+  // list.before(video);
+
+  let videoSourcesSelect = document.getElementById("video-source");
+let videoPlayer = document.getElementById("player");
+
+// Create Helper to ask for permission and list devices
+let MediaStreamHelper = {
+    // Property of the object to store the current stream
+    _stream: null,
+    // This method will return the promise to list the real devices
+    getDevices: function() {
+        return navigator.mediaDevices.enumerateDevices();
+    },
+    // Request user permissions to access the camera and video
+    requestStream: function() {
+        if (this._stream) {
+            this._stream.getTracks().forEach(track => {
+                track.stop();
+            });
+        }
+        const videoSource = videoSourcesSelect.value;
+        const constraints = {
+            video: {
+                deviceId: videoSource ? {exact: videoSource} : undefined
+            }
+        };
+    
+        return navigator.mediaDevices.getUserMedia(constraints);
+    }
+};
+
+// Request streams (audio and video), ask for permission and display streams in the video element
+MediaStreamHelper.requestStream().then(function(stream){
+  console.log(stream);
+  // Store Current Stream
+  MediaStreamHelper._stream = stream;
+
+  // Select the Current Streams in the list of devices
+  videoSourcesSelect.selectedIndex = [...videoSourcesSelect.options].findIndex(option => option.text === stream.getVideoTracks()[0].label);
+
+  // Play the current stream in the Video element
+  videoPlayer.srcObject = stream;
+  
+  // You can now list the devices using the Helper
+  MediaStreamHelper.getDevices().then((devices) => {
+      // Iterate over all the list of devices (InputDeviceInfo and MediaDeviceInfo)
+      devices.forEach((device) => {
+          let option = new Option();
+          option.value = device.deviceId;
+
+          // According to the type of media device
+          switch(device.kind){
+              // Append device to list of Cameras
+              case "videoinput":
+                  option.text = device.label || `Camera ${videoSourcesSelect.length + 1}`;
+                  videoSourcesSelect.appendChild(option);
+                  break;
+          }
+
+          console.log(device);
+      });
+  }).catch(function (e) {
+      console.log(e.name + ": " + e.message);
   });
-
-  const video = document.createElement("video");
-  video.srcObject = mediaStream;
-  video.autoplay = true;
-  video.videoWidth = "300px";
-  video.videoHeight = "300px"
-
-  list.before(video);
+}).catch(function(err){
+  console.error(err);
+}); 
 
 
   function render() {
